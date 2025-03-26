a87f6de6c8bad5b737c933cf89d136a2e5f2ef93
igorpxch <igorsalvador.pacheco@gmail.com>
2025-03-25 21:40:33 -0300

    Create Protocolo.java

---
+++ Protocolo.java
@@ -0,0 +1,31 @@
+// Classe Protocolo
+public class Protocolo {
+    private String assunto;
+    private String data;
+    private String horario;
+
+    // Getters e Setters
+    public String getAssunto() {
+        return assunto;
+    }
+
+    public void setAssunto(String assunto) {
+        this.assunto = assunto;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+}
