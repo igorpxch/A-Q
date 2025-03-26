398446b3adde32bd5c356fad68d06e5e2d806b55
igorpxch <igorsalvador.pacheco@gmail.com>
2025-03-25 21:39:31 -0300

    Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,13 @@
+// Classe Cliente (herda de Usuario)
+public class Cliente extends Usuario {
+    private String cpf;
+
+    // Getter e Setter
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
