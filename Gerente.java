32de2af9f6837f2a563028950009631ad94727dd
igorpxch <igorsalvador.pacheco@gmail.com>
2025-03-25 21:40:08 -0300

    Create Gerente.java

---
+++ Gerente.java
@@ -0,0 +1,14 @@
+
+// Classe Gerente (herda de Usuario)
+public class Gerente extends Usuario {
+    private String cnpj;
+
+    // Getter e Setter
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+}
