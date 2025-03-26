235f0556e7301ffb42873dd4d2a3292d95c3e97e
igorpxch <igorsalvador.pacheco@gmail.com>
2025-03-25 21:39:51 -0300

    Create Atendente.java

---
+++ Atendente.java
@@ -0,0 +1,13 @@
+// Classe Atendente (herda de Usuario)
+public class Atendente extends Usuario {
+    private String matricula;
+
+    // Getter e Setter
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+}
